8837601fc279e026808b2eff0a569b768b916af9
Sergey Storchay <r8@r8.com.ua>
2015-03-29 12:05:15 +0300

    Add attributes for command line options generation

--- attributes/default.rb
+++ attributes/default.rb
@@ -34,3 +34,31 @@ default['mailhog']['binary']['path'] = '/usr/local/bin/MailHog'
 default['mailhog']['binary']['prefix_url'] = 'https://github.com/mailhog/MailHog/releases/download/v'
 default['mailhog']['binary']['checksum']['linux_386'] = 'a72d1016b70964562c8a77a3b57637a77889ee61f3b22973e0a7beb17181d8da'
 default['mailhog']['binary']['checksum']['linux_amd64'] = 'e8e9acb4fa4470f4d4c3a4bba312f335bfc28122ea723599531699f099b4c9a5'
+
+default['mailhog']['smtp']['ip']       = nil # Default: 0.0.0.0
+default['mailhog']['smtp']['port']     = nil # Default: 1025
+default['mailhog']['smtp']['outgoing'] = nil
+
+default['mailhog']['api']['ip']   = nil # Default: 0.0.0.0
+default['mailhog']['api']['port'] = nil # Default: 8025
+
+default['mailhog']['ui']['ip']   = nil # Default: 0.0.0.0
+default['mailhog']['ui']['port'] = nil # Default: 8025
+
+default['mailhog']['cors-origin'] = nil
+default['mailhog']['hostname']    = nil # Default: mailhog.example
+
+default['mailhog']['jim']['enable']              = nil # Default: false
+default['mailhog']['jim']['accept']              = nil # Default: 0.99
+default['mailhog']['jim']['disconnect']          = nil # Default: 0.005
+default['mailhog']['jim']['linkspeed']['affect'] = nil # Default: 0.1
+default['mailhog']['jim']['linkspeed']['max']    = nil # Default: 10240
+default['mailhog']['jim']['linkspeed']['min']    = nil # Default: 1024
+default['mailhog']['jim']['reject']['auth']      = nil # Default: 0.05
+default['mailhog']['jim']['reject']['recipient'] = nil # Default: 0.05
+default['mailhog']['jim']['reject']['sender']    = nil # Default: 0.05
+
+default['mailhog']['storage']               = nil # Default: memory
+default['mailhog']['mongodb']['uri']        = nil # Default: 127.0.0.1:27017
+default['mailhog']['mongodb']['db']         = nil # Default: mailhog
+default['mailhog']['mongodb']['collection'] = nil # Default: messages
